4f043109a78caf16707a41e79dd90d7c9bee2308
fummicc1 <fumiya.tennis1@gmail.com>
2023-12-31 02:05:49 +0900

    Create search repositories feature

---
+++ application/lib/features/search_repositories/route.dart
@@ -0,0 +1,19 @@
+import 'package:application/features/repository_detail/route.dart';
+import 'package:application/features/search_repositories/page.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:go_router/go_router.dart';
+
+part 'route.g.dart';
+
+@TypedGoRoute<SearchRepositoriesRoute>(
+  path: "/search",
+  routes: [
+    TypedGoRoute<RepositoryDetailRoute>(path: "repository/:repositoryId")
+  ],
+)
+class SearchRepositoriesRoute extends GoRouteData {
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return const SearchRepositoriesPage();
+  }
+}
