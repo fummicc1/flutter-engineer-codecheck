0cbc39199ba954b03576e3d2bcdfe8224be9bf7d
fummicc1 <fumiya.tennis1@gmail.com>
2023-12-31 02:03:51 +0900

    Create theme

---
+++ application/lib/theme/app_color.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_color.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appColorHash() => r'c243e79ff63fca892a3c15b9eaa460a1b109082e';
+
+/// See also [appColor].
+@ProviderFor(appColor)
+final appColorProvider = Provider<AppColor>.internal(
+  appColor,
+  name: r'appColorProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$appColorHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AppColorRef = ProviderRef<AppColor>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
