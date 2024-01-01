1678cc4377d810357574cca994fa6e674a361a1e
fummicc1 <fumiya.tennis1@gmail.com>
2024-01-01 14:03:44 +0900

    Create RepositoryService.

---
+++ application/lib/services/repository.service.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'repository.service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$repositoryServiceHash() => r'e5dce8c3493eb7378e440b85629084c0980d8660';
+
+/// See also [repositoryService].
+@ProviderFor(repositoryService)
+final repositoryServiceProvider = Provider<RepositoryService>.internal(
+  repositoryService,
+  name: r'repositoryServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$repositoryServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef RepositoryServiceRef = ProviderRef<RepositoryService>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
