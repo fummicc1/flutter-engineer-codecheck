9515f436089675c2845b444859eae21dff62b8e5
fummicc1 <fumiya.tennis1@gmail.com>
2024-01-01 14:03:18 +0900

    Create Repository model

---
+++ application/lib/models/repository.model.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'repository.model.freezed.dart';
+part 'repository.model.g.dart';
+
+@freezed
+sealed class Repository with _$Repository {
+  factory Repository({
+    required String name,
+    required String description,
+  }) = _Repository;
+
+  factory Repository.fromJson(Map<String, dynamic> json) =>
+      _$RepositoryFromJson(json);
+}
---
+++ application/lib/models/repository.model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'repository.model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$RepositoryImpl _$$RepositoryImplFromJson(Map<String, dynamic> json) =>
+    _$RepositoryImpl(
+      name: json['name'] as String,
+      description: json['description'] as String,
+    );
+
+Map<String, dynamic> _$$RepositoryImplToJson(_$RepositoryImpl instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'description': instance.description,
+    };
