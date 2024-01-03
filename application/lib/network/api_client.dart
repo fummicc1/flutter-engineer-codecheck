b5faa0308e90545e2c2499cf4ca5ee6f20758906
fummicc1 <fumiya.tennis1@gmail.com>
2024-01-03 15:24:13 +0900

    Create APIClient to perform unit-testing

---
+++ application/lib/network/api_client.dart
@@ -0,0 +1,32 @@
+import 'package:application/network/request.dart';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart';
+
+class APIClient {
+  Future send<R extends NetworkRequest>(R request) async {
+    final method = request.method;
+    final uri = request.uri;
+    final headers = request.headers;
+    final body = request.body;
+    if (kDebugMode) {
+      String curl = 'curl -X $method';
+      for (final header in headers.entries) {
+        curl += ' -H "${header.key}: ${header.value}"';
+      }
+      if (method != "GET" && body.isNotEmpty) {
+        curl += ' -d \'${body.toString()}\'';
+      }
+      curl += ' $uri';
+      print(curl);
+    }
+    if (method == 'GET') {
+      final response = await get(uri, headers: headers);
+      return response;
+    } else if (method == 'POST') {
+      final response = await post(uri, headers: headers, body: body);
+      return response;
+    } else {
+      throw Exception('Unsupported HTTP method: $method');
+    }
+  }
+}
--- application/lib/services/repository.service.dart
+++ application/lib/services/repository.service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:application/models/repository.model.dart';
+import 'package:application/network/api_client.dart';
 import 'package:application/network/github_repository.request.dart';
 import 'package:flutter/foundation.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -8,12 +9,21 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'repository.service.g.dart';
 
 @Riverpod(keepAlive: true)
-RepositoryService repositoryService(ref) => RepositoryService();
+RepositoryService repositoryService(ref) => RepositoryService(
+      apiClient: APIClient(),
+    );
 
 class RepositoryService {
-  Future<List<Repository>> getRepositories({required String query}) async {
+  RepositoryService({required APIClient apiClient}) : _apiClient = apiClient;
+
+  final APIClient _apiClient;
+
+  Future<List<Repository>>
+      getRepositories<R extends GetGitHubRepositoryRequest>(
+          {required String query}) async {
     try {
-      final response = await GetGitHubRepositoryRequest(query: query).send();
+      final request = GetGitHubRepositoryRequest(query: query);
+      final response = await _apiClient.send(request);
       if (response.statusCode == 200) {
         final Map<String, dynamic> repositoriesJson =
             json.decode(response.body);
