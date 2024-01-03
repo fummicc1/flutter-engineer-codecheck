efd6109dde035a016f80e5c604ad4d2ebb7d45e0
fummicc1 <fumiya.tennis1@gmail.com>
2024-01-03 18:18:00 +0900

    Fix test helper

--- application/test/helpers/repository.dart
+++ application/test/helpers/repository.dart
@@ -6,6 +6,9 @@ class RepositoryTestHelper {
     return List.generate(count, (index) {
       return Repository(
         id: index + 1,
+        owner: RepositoryOwner(
+          login: faker.person.name(),
+        ),
         name: faker.lorem.word(),
         description: faker.lorem.sentences(3).join("\n"),
       );
