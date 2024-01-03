371f1a72c7f3a76c4f66b693b4a956d8be225dd0
fummicc1 <fumiya.tennis1@gmail.com>
2024-01-03 17:46:00 +0900

    Create FeaturePage

--- application/lib/features/search_repositories/state.dart
+++ application/lib/features/search_repositories/state.dart
@@ -1,10 +1,13 @@
 import 'package:application/models/repository.model.dart';
+import 'package:application/ui_components/page.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'state.freezed.dart';
 
 @freezed
-class SearchRepositoriesState with _$SearchRepositoriesState {
+class SearchRepositoriesState
+    with _$SearchRepositoriesState
+    implements FeatureState {
   const factory SearchRepositoriesState({
     @Default("Flutter") String query,
     required List<Repository> repositories,
