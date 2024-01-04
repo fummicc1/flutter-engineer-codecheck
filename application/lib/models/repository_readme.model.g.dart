300fa6b05199da5741ca4df10b6c3594021a0866
fummicc1 <fumiya.tennis1@gmail.com>
2024-01-04 22:33:56 +0900

    Create RepositoryReadme model

---
+++ application/lib/models/repository_readme.model.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'repository_readme.model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$RepositoryReadMeImpl _$$RepositoryReadMeImplFromJson(
+        Map<String, dynamic> json) =>
+    _$RepositoryReadMeImpl(
+      owner: json['owner'] as String,
+      name: json['name'] as String,
+      content: json['content'] as String,
+    );
+
+Map<String, dynamic> _$$RepositoryReadMeImplToJson(
+        _$RepositoryReadMeImpl instance) =>
+    <String, dynamic>{
+      'owner': instance.owner,
+      'name': instance.name,
+      'content': instance.content,
+    };
