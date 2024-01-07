d46b07473138925e008b1d751628254f3f9efb22
fummicc1 <fumiya.tennis1@gmail.com>
2024-01-07 23:04:56 +0900

    Create RepositoryDetailController to perform test.

---
+++ application/lib/features/repository_detail/controller.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$repositoryDetailControllerHash() =>
+    r'782c2a56f6d05079ad0e95dff49b15595ed3a7a0';
+
+/// See also [repositoryDetailController].
+@ProviderFor(repositoryDetailController)
+final repositoryDetailControllerProvider =
+    AutoDisposeProvider<RepositoryDetailController>.internal(
+  repositoryDetailController,
+  name: r'repositoryDetailControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$repositoryDetailControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef RepositoryDetailControllerRef
+    = AutoDisposeProviderRef<RepositoryDetailController>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
