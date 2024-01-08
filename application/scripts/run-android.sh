2134be269dc0ed7d158b64900421240c52132f8b
fummicc1 <fumiya.tennis1@gmail.com>
2024-01-08 17:45:23 +0900

    Delete unused script

--- application/scripts/run-android.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-targetDeviceName="sdk gphone arm64"
-fvm flutter run -d "$targetDeviceName" --dart-define-from-file=config.json
--- application/scripts/run-ios.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-targetDeviceName="iPhone 15 Pro"
-fvm flutter emulators --launch ios
-fvm flutter run -d "$targetDeviceName" --dart-define-from-file=config.json
